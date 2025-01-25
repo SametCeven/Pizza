94864813f693944a6b2d5346d5c4d0fa222b3660
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-01-25 22:07:48 +0300

    first commit
    - initial setup
    - added libraries
     reactstrap / bootstrap
     mui
     cypress

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
-import './index.css'
+import "./reset.css"
+import './main.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

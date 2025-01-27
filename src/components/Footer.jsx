9229222d5ada9b56e03c4ab61fd2d7b2326513c0
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-01-27 23:41:24 +0300

    added mobile css for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import "./Footer.css"
 import {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer(){
     const menuItems = ["Terminal Pizza","5 Kişilik Hackathlon Pizza","useEffect Tavuklu Pizza","Beyaz Console Frosty","Testler Geçti Mutlu Burger","Position Absoulte Acı Burger"];
@@ -9,40 +11,47 @@ export default function Footer(){
 
     return(
         <footer className="footer">
-            <div className="footer-info">
-                <img id="footer-icon-logo" src="../images/iteration-2-images/footer/logo-footer.svg" alt="footer-logo" />
-                <div>
-                    <img src="../images/iteration-2-images/footer/icons/icon-1.png" alt="footer-icon-map" />
-                    <p>341 Londonderry Road, Istanbul Turkey</p>
+            <div className="footer-sub">
+                <div className="footer-info">
+                    <img id="footer-icon-logo" src="../images/iteration-2-images/footer/logo-footer.svg" alt="footer-logo" />
+                    <div>
+                        <img src="../images/iteration-2-images/footer/icons/icon-1.png" alt="footer-icon-map" />
+                        <p>341 Londonderry Road, Istanbul Turkey</p>
+                    </div>
+                    <div>
+                        <img src="../images/iteration-2-images/footer/icons/icon-2.png" alt="footer-icon-email" />
+                        <span>aciktim@teknolojikyemekler.com</span>
+                    </div>
+                    <div>
+                        <img src="../images/iteration-2-images/footer/icons/icon-3.png" alt="footer-icon-phone" />
+                        <span>+90 216 123 45 67</span>
+                    </div>
                 </div>
-                <div>
-                    <img src="../images/iteration-2-images/footer/icons/icon-2.png" alt="footer-icon-email" />
-                    <span>aciktim@teknolojikyemekler.com</span>
+
+                <div className="footer-menu">
+                    <h1>Hot Menu</h1>
+                    {menu.map((men,index)=>{
+                        return(
+                            <span key={index} >{men}</span>
+                        )
+                    })}
                 </div>
-                <div>
-                    <img src="../images/iteration-2-images/footer/icons/icon-3.png" alt="footer-icon-phone" />
-                    <span>+90 216 123 45 67</span>
+
+                <div className="footer-insta">
+                    <h1>Instagram</h1>
+                    {imgs.map((img,index)=>{
+                        let tempSrc = `../images/iteration-2-images/footer/insta/${img}`;
+                        return( 
+                            <img key={index} src={tempSrc}/>
+                        )
+                    })}
                 </div>
             </div>
 
-            <div className="footer-menu">
-                <h1>Hot Menu</h1>
-                {menu.map((men,index)=>{
-                    return(
-                        <span key={index} >{men}</span>
-                    )
-                })}
-            </div>
-
-            <div className="footer-insta">
-                <h1>Instagram</h1>
-                {imgs.map((img,index)=>{
-                    let tempSrc = `../images/iteration-2-images/footer/insta/${img}`;
-                    return( 
-                        <img key={index} src={tempSrc}/>
-                    )
-                })}
-            </div>
+            <div className="footer-span">
+                    <span>2023 Teknolojik Yemekler.</span>
+                    <FontAwesomeIcon icon={faTwitter} />
+                </div>
         </footer>
 
     )

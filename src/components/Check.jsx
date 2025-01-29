1b9363d9c01340b8585c73f4152a296814f14e01
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-01-29 17:27:07 +0300

    chekbox revised

--- src/components/Check.jsx
+++ src/components/Check.jsx
@@ -2,14 +2,17 @@ export default function Check(props){
     const {topping,handleChange} = props;
     return(
         <div className="input-check">
-            <input 
-                type="checkbox" 
-                name="toppings"
-                id={topping}
-                value={topping}
-                onChange={handleChange}
-                />
-            <label htmlFor={topping}>{topping}</label>
+            <label className="check-l" htmlFor={topping}> {topping}
+                <input 
+                    className="check-i"
+                    type="checkbox" 
+                    name="toppings"
+                    id={topping}
+                    value={topping}
+                    onChange={handleChange}
+                    />
+                <span className="check-s"></span>
+            </label>
         </div>
     )
 }
